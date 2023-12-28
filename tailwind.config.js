9391e15238399634e08ff4f952f1f2ce1beaccf2
kxng <iluhjosh13@gmail.com>
2023-12-29 00:16:27 +0100

    Forgot to add a typescript file.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,7 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   relative: true,
-  content: ["./public/index.html", "./src/**/*.{ts}"],
+  content: ["./public/index.html", "./src/**/*.ts"],
   theme: {
     extend: {},
   },
