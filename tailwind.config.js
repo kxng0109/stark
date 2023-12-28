c28e6f469959b012750502162805af2fccad6486
kxng <iluhjosh13@gmail.com>
2023-12-29 00:07:55 +0100

    Config plus a few more

---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  relative: true,
+  content: ["./public/index.html", "./src/**/*.{ts}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
