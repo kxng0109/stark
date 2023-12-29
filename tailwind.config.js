f3913e16e83d88e15334648feed5115e8481c45d
kxng <iluhjosh13@gmail.com>
2023-12-29 21:31:54 +0100

    Styled the navbar for mobile

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,15 @@ module.exports = {
   relative: true,
   content: ["./public/index.html", "./src/**/*.ts"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'yellow-white': '#f6f6eb',
+        'gold': '#f0d062',
+        'blue': '#381fd1',
+        'hover-blue': '#4b34d4',
+        'dark-blue': '#10284b',
+      }
+    },
   },
   plugins: [],
 }
