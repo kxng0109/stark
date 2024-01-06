1d347790dee364183e2cf8dcce387e10c2da7558
kxng <iluhjosh13@gmail.com>
2024-01-06 02:34:46 +0100

    Worked on screens lower than 1024px, though I need to do something with the navbar.

--- tailwind.config.js
+++ tailwind.config.js
@@ -18,7 +18,8 @@ module.exports = {
       screens: {
         'xs' : '460px',
         'md' : '841px',
-        'md+': '850px'
+        'md+': '850px',
+        'md++': '980px'
       },
       zIndex:{
         '1' : '1',
